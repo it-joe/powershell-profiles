3844f070795c5c8d30f0bb98245e91b1aa0b1bbd
it-joe <joku@it-joe.de>
2020-09-13 16:25:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# PowerShell and Windows PowerShell profiles
+Author: [it-joe](https://github.com/it-joe)
+
+> My PowerShell and Windows PowerShell profiles.
+
+## PowerShell
+Includes *Microsoft.PowerShell_profile.ps1*.
+
+Save to location:
+```
+C:\Users\<User>\Documents\PowerShell
+```
+
+## Windows PowerShell
+Includes *Microsoft.PowerShell_profile.ps1*.
+
+Save to location:
+```
+C:\Users\<User>\Documents\WindowsPowerShell
+```
---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+#region Set aliases for directories
+function Set-Repos { 
+    $Path = Join-Path -Path $env:SystemDrive -ChildPath Sources | Join-Path -ChildPath Repos
+    Set-Location -Path $Path
+}
+Set-Alias -Name Repos -Value Set-Repos
+Set-Location C:\Repos
+#endregion
+
+#region ASCII Art
+$asciiArt = "
+ICAgICAgICAgICAgICAgICAgIExPQURJTkcuLi4gICAgICAgICAgICAgICAgICAK4pWU4pWQ4pWQ
+4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ
+4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ
+4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWXCuKVkSAwJSDilojilojilojilojilojilojilojilojiloji
+lojilojilojilojilojilojilojilojilojilojilojilojilojilojilojilojilojilojiloji
+lojilojilojilojilojilojilojiloggMTAwJSDilZEgCuKVmuKVkOKVkOKVkOKVkOKVkOKVkOKV
+kOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKV
+kOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKV
+kOKVkOKVnQogICAgICAgICAgICAgICAgICAgQ09NUExFVEVEISAg"
+Write-Host $([system.text.encoding]::UTF8.GetString([system.convert]::FromBase64String($asciiArt))) -ForegroundColor Red
+Write-Host "`n"
+#endregion
