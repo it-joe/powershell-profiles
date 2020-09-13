95218e8bf5fc891e431932d05cf3365a231d27ab
it-joe <joku@it-joe.de>
2020-09-13 16:26:23 +0200

    FEAT: Add profiles

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,38 @@
+#region Set aliases for directories
+function Set-Sources { 
+    $Path = Join-Path -Path $env:SystemDrive -ChildPath Sources
+    Set-Location -Path $Path 
+}
+Set-Alias -Name Sources -Value Set-Sources
+
+function Set-Repos { 
+    $Path = Join-Path -Path $env:SystemDrive -ChildPath Sources | Join-Path -ChildPath Repos
+    Set-Location -Path $Path
+}
+Set-Alias -Name Repos -Value Set-Repos
+
+Set-Location C:\Sources\Repos
+#endregion
+
+#region Prompt
+function Prompt {
+    $StrPrompt = "PS " + $(Get-Location) + ">"
+    Write-Host $StrPrompt -NoNewline -ForegroundColor Cyan
+    return " "
+}
+#endregion
+
+#region ASCII Art
+$asciiArt = "
+ICAgICAgICAgICAgICAgICAgIExPQURJTkcuLi4gICAgICAgICAgICAgICAgICAK4pWU4pWQ4pWQ
+4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ
+4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ
+4pWQ4pWQ4pWQ4pWQ4pWQ4pWQ4pWXCuKVkSAwJSDilojilojilojilojilojilojilojilojiloji
+lojilojilojilojilojilojilojilojilojilojilojilojilojilojilojilojilojilojiloji
+lojilojilojilojilojilojilojiloggMTAwJSDilZEgCuKVmuKVkOKVkOKVkOKVkOKVkOKVkOKV
+kOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKV
+kOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKVkOKV
+kOKVkOKVnQogICAgICAgICAgICAgICAgICAgQ09NUExFVEVEISAg"
+Write-Host $([system.text.encoding]::UTF8.GetString([system.convert]::FromBase64String($asciiArt))) -ForegroundColor Cyan
+Write-Host "`n"
+#endregion
